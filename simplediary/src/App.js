ec9feddac3d717c4036a4a1e56a7785cf1876f7e
junwoo <junwoo1357@gmail.com>
2023-11-02 15:03:21 +0900

    데이터 삭제, 수정

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,12 +1,67 @@
-import {useState} from 'react'
+import { useState, useRef } from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
-import './App.css';
-import DiaryEditor from './DiaryEditor';
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "JW",
+//     content: "일기내용",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "Hizen",
+//     content: "일기내용2",
+//     emotion: 4,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "WJW",
+//     content: "일기내용3",
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onRemove = (targetId) => {
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+
   return (
-    <div className='App'>
-      <DiaryEditor/>
+    <div className="App">
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
 }
--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -1,50 +1,76 @@
-import {useState} from 'react';
+import React, { useRef, useState } from "react";
 
-const DiaryEditor = () =>{
-    const [state, setState] = useState({
-        author: "",
-        content: "",
-        emotion: 1,
+const DiaryEditor = ({ onCreate }) => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const authorInput = useRef();
+  const contentInput = useRef();
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
     });
-
-    const handleChangeState = (e) =>{
-        setState({
-            ...state,
-            [e.target.name]: e.target.value,
-        })
+  };
+  const handleSubmit = () => {
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return;
     }
-    const handleSubmit = () =>{
-        console.log(state);
-        alert('저장성공');
+    if (state.content.length < 5) {
+      contentInput.current.focus();
+      return;
     }
+    onCreate(state.author, state.content, state.emotion);
+    alert("저장 성공");
+    setState({
+      author: "",
+      content: "",
+      emotion: 1,
+    });
+  };
 
-return <div className="DiaryEditor">
-    <h2>오늘의 일기</h2>
-    <div>
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
         <input
-        name = 'author'
-        value={state.author}
-        onChange={handleChangeState}/>
-    </div>
-    <div>
+          ref={authorInput}
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
         <textarea
-        name='content'
-        value={state.content}
-        onChange={handleChangeState}/>
-    </div>
-    <div>
+          ref={contentInput}
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
         오늘의 감정점수 :
-        <select name='option' value={state.emotion} onChange={handleChangeState}>
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-            <option value={4}>4</option>
-            <option value={5}>5</option>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
         </select>
-    </div>
-    <div>
+      </div>
+      <div>
         <button onClick={handleSubmit}>저장하기</button>
+      </div>
     </div>
-    </div>;
-}
-export default DiaryEditor
+  );
+};
+export default DiaryEditor;
